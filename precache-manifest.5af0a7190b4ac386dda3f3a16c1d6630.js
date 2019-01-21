f7e98928aca1dbaaa4ac20e67b52b3a541eefa0b
Nhan Pham <nhanpham1621@gmail..com>
2019-01-21 10:59:11 -0600

    Updates

---
+++ precache-manifest.5af0a7190b4ac386dda3f3a16c1d6630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "01eee281c920440e04fe",
+    "url": "/robofriends/static/js/main.01eee281.chunk.js"
+  },
+  {
+    "revision": "b5aae44bba93dd3576c8",
+    "url": "/robofriends/static/js/1.b5aae44b.chunk.js"
+  },
+  {
+    "revision": "01eee281c920440e04fe",
+    "url": "/robofriends/static/css/main.a3145b43.chunk.css"
+  },
+  {
+    "revision": "b5aae44bba93dd3576c8",
+    "url": "/robofriends/static/css/1.fdb72aa2.chunk.css"
+  },
+  {
+    "revision": "0c2ae01c893b29aea56e7b2c7b7521a8",
+    "url": "/robofriends/index.html"
+  }
+];
